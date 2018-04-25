5fb6204596bfe31d66b56be5397da25ec7561c61
miltonf <miltonf@monerov.org>
2018-04-25 03:43:36 +0300

    update config

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
-#define CURRENT_TRANSACTION_VERSION                     2
+#define CURRENT_TRANSACTION_VERSION                     3
 #define CURRENT_BLOCK_MAJOR_VERSION                     1
 #define CURRENT_BLOCK_MINOR_VERSION                     0
 
@@ -13,4 +13,4 @@
 
 
 #define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            60
-#define CRYPTONOTE_DISPLAY_DECIMAL_POINT                12
+#define CRYPTONOTE_DISPLAY_DECIMAL_POINT                11
